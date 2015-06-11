f4d78a8c48d7104972a2a07cea4e86477ef3b49f
Carlos <sanchezseijas85@hotmail.com>
2015-06-11 16:15:34 +0200

    autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,34 @@
 var models = require('../models/models.js');
 
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if(quiz) {
+				req.quiz = quiz;
+				next();
+			}else{
+				next(new Error('No existe quizId=' + quizId));
+			}
+		}
+	).catch(function(error) {next(error);});
+	
+};
+
+
+
 exports.answer = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		 if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', { quiz:quiz, respuesta: "Correcto" });  
+		 if (req.query.respuesta === req.quiz.respuesta){
+			res.render('quizes/answer', { quiz:req.quiz, respuesta: "Correcto" });  
 		 }else{
-			res.render('quizes/answer', { quiz:quiz, respuesta: "Incorrecto" });
+			res.render('quizes/answer', { quiz:req.quiz, respuesta: "Incorrecto" });
 		 };
 	});
 };
 
 exports.show = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', { quiz: quiz });  
+		res.render('quizes/show', { quiz: req.quiz });  
 	});
 };
   
